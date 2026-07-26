39d7d1f0c38350cad5511f13ba9cecf65778775c
hirot <hirotosekimoto@icloud.com>
2026-07-26 09:00:15 +0900

    Disable iOS native rendering on category headings

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'tdr-archive-'
-const CACHE_NAME = `${CACHE_PREFIX}v59-coordinate-precision`
+const CACHE_NAME = `${CACHE_PREFIX}v60-ios-button-surface`
 const scopeUrl = self.registration.scope
 const appUrl = (path) => new URL(path, scopeUrl).toString()
 const APP_SHELL = [
