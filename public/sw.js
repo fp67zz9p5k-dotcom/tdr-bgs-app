e4c9c31e996464889341d3d52cc674fe6889ca1d
hirot <hirotosekimoto@icloud.com>
2026-07-26 10:17:49 +0900

    Render iOS category border on masked surface

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'tdr-archive-'
-const CACHE_NAME = `${CACHE_PREFIX}v65-ios-antialiased-heading-border`
+const CACHE_NAME = `${CACHE_PREFIX}v66-ios-masked-heading-surface`
 const scopeUrl = self.registration.scope
 const appUrl = (path) => new URL(path, scopeUrl).toString()
 const APP_SHELL = [
