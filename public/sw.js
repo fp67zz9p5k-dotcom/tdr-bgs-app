9e3b829dd8ccdd04e12c9205fc7c6c612e9f9d37
hirot <hirotosekimoto@icloud.com>
2026-07-26 10:36:49 +0900

    Unify dark detail view colors

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'tdr-archive-'
-const CACHE_NAME = `${CACHE_PREFIX}v68-ios-inset-heading-stroke`
+const CACHE_NAME = `${CACHE_PREFIX}v69-dark-detail-surfaces`
 const scopeUrl = self.registration.scope
 const appUrl = (path) => new URL(path, scopeUrl).toString()
 const APP_SHELL = [
